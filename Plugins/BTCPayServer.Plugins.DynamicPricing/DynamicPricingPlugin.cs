ceb41741ddcda34c6a5936f05132cf9c8b876405
æon <7865680+aeonBTC@users.noreply.github.com>
2025-03-28 17:54:04 -0700

    Add files via upload

--- Plugins/BTCPayServer.Plugins.DynamicPricing/DynamicPricingPlugin.cs
+++ Plugins/BTCPayServer.Plugins.DynamicPricing/DynamicPricingPlugin.cs
@@ -7,20 +7,9 @@ namespace BTCPayServer.Plugins.DynamicPricing
 {
     public class DynamicPricingPlugin : BaseBTCPayServerPlugin
     {
-        public override string Identifier => "BTCPayServer.Plugins.DynamicPricing";
-        public override string Name => "Dynamic Pricing";
-        public override string Description => "Apply shipping and percentage-based discounts based on order total thresholds.";
-        public override string Author => "aeonBTC";
-        public override string Version => "1.0.0";
-
         public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
         {
             new() { Identifier = nameof(BTCPayServer), Condition = ">=2.0.0" }
-        };
-
-        public override PluginResource[] Resources { get; } = new []
-        {
-            new PluginResource("Source Code", "https://github.com/aeonBTC/BTCPayServerPlugins/Plugins/BTCPayServer.Plugins.DynamicPricing")
         };
 
         public override void Execute(IServiceCollection applicationBuilder)
